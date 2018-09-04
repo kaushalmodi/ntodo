287edb5dc4e7d715191f5103852112f4a5dfd420
Kaushal Modi <kaushal.modi@gmail.com>
2018-09-04 11:56:50 -0400

    Change default priority to 4

--- src/ntodopkg/globals.nim
+++ src/ntodopkg/globals.nim
@@ -7,7 +7,7 @@ type
 const
   tokenFileName* = ".todoist-token"
   apiBaseUrl* = "https://beta.todoist.com/API/v8"
-  defaultPriority*: Priority = 1 ## Default priority when creating tasks
+  defaultPriority*: Priority = 4 ## Default priority when creating tasks
   defaultDueLang* = "en"         ## Default language to be used for parsing "due string" when creating tasks
 
 var
